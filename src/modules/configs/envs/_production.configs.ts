e7e926357bd4193eb5026c97df4c9c9482cffea4
Mai Trần Gia Hân <88010632+Hanmai001@users.noreply.github.com>
2024-07-28 13:07:52 +0700

    update config

--- src/modules/configs/envs/_production.configs.ts
+++ src/modules/configs/envs/_production.configs.ts
@@ -6,9 +6,9 @@ const { publicRuntimeConfig } = getNextConfig();
 export const productionConfigs: Configs = {
   URL_CROSS_STORAGE: 'https://hanmai001.github.io/cross-storage/index.html',
   // URL_CROSS_STORAGE: publicRuntimeConfig.URL_CROSS_STORAGE || "http://127.0.0.1:5500/index.html",
-  URL_MAIN_API: publicRuntimeConfig.URL_MAIN_API || "http://blockclip.pro.vn:4000/",
-  SOCKET_ENDPOINT: publicRuntimeConfig.SOCKET_ENDPOINT || "http://blockclip.pro.vn:5000/",
-  PUBLIC_URL: publicRuntimeConfig.PUBLIC_URL || "https://blockclip.pro.vn/",
+  URL_MAIN_API: publicRuntimeConfig.URL_MAIN_API || "http://blockclip.pro.vn:4000",
+  SOCKET_ENDPOINT: publicRuntimeConfig.SOCKET_ENDPOINT || "http://blockclip.pro.vn:5000",
+  PUBLIC_URL: publicRuntimeConfig.PUBLIC_URL || "https://blockclip.pro.vn",
   chains: {
     [ChainId.BSC_TESTNET]: {
       erc20s: {
